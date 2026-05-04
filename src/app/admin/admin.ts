dc87b5bc194d1b44c14366502cc2efd64d842cfb
Faisa Hassan <faisaah917@gmail.com>
2026-05-04 09:06:17 -0500

    added admin dashboradw

---
+++ src/app/admin/admin.ts
@@ -0,0 +1,55 @@
+import { Component, inject, signal } from '@angular/core';
+import { collection, onSnapshot, doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { DiscussionService } from '../discussion.service';
+import { DatePipe } from '@angular/common';
+
+interface User {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  isActive: boolean;
+}
+
+@Component({
+  selector: 'app-admin',
+  imports: [DatePipe],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css',
+})
+export class AdminComponent {
+
+  discussionService = inject(DiscussionService);
+
+  users = signal<User[]>([]);
+
+  constructor() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    const userCollection = collection(db, 'users');
+
+    onSnapshot(userCollection, snapshot => {
+      const data = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      })) as User[];
+
+      this.users.set(data);
+    });
+  }
+
+  deactivateUser(id: string) {
+    updateDoc(doc(db, 'users', id), {
+      isActive: false
+    });
+  }
+
+  deletePost(id: string) {
+    this.discussionService.deleteDiscussion(id);
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HabitsComponent } from './habits/habits';
 import { DiscussionComponent } from './discussion/discussion';
+import { AdminComponent } from './admin/admin';
 
 export const routes: Routes = [
     { path: 'habit', component: HabitsComponent },
     { path: 'discussion', component: DiscussionComponent },
+    { path: 'admin', component: AdminComponent }
 
 ];
